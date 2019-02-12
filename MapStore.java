ebd2beaa06e0dffe65e59eab753d8ad5d758b5cf
blackevil2703 <45650551+blackevil2703@users.noreply.github.com>
2019-02-12 18:19:37 +0530

    12/2/2019

--- MapStore.java
+++ MapStore.java
@@ -7,38 +7,82 @@ public class MapStore
 
 	public static void main(String[] args) 
 	{
+		
 		HashMap<Integer,Object>rc=new HashMap<Integer,Object>();
 		
-		List al=new ArrayList();
-		al.add("a");
 		
 		
-		rc.put(100, new RationCard("A",30,"Chennai",al));
-		rc.put(101, new RationCard("B",40,"Chennai",al));
-		rc.put(102, new RationCard("A",30,"Chennai",al));
-		rc.put(103, new RationCard("C",25,"Chennai",al));
 		
+		List<RationCard> al=new ArrayList<RationCard>();
+		al.add(new RationCard("a",27));
+		al.add(new RationCard("aa",1));
+		rc.put(100, new RationCard("A",30,"Adayar, Chennai",al));
+		al=new ArrayList<RationCard>();
+		
+		
+		al.add(new RationCard("b",36));
+		al.add(new RationCard("bb",5));
+		rc.put(101, new RationCard("B",40,"Vadapalani, Chennai",al));
+		al=new ArrayList<RationCard>();
+		
+		al.add(new RationCard("a",27));
+		al.add(new RationCard("aa",1));
+		rc.put(102, new RationCard("A",30,"Adayar, Chennai",al));
+		al=new ArrayList<RationCard>();
+		
+		al.add(new RationCard("c",21));
+		al.add(new RationCard("cc",0));
+		rc.put(103, new RationCard("C",25,"Guindy, Chennai",al));
+		al=new ArrayList<RationCard>();
+		
+		
+		
+		List<Object> listq = new ArrayList<Object>(rc.values());
+		
+		System.out.println(listq.size());
 		
 		Set s1=rc.entrySet();
+		
+		 
 		
 		Iterator itr=s1.iterator();
 		
 		while(itr.hasNext())
 		{
 			Map.Entry m1=(Map.Entry)itr.next();
-	    	System.out.println(m1.getKey()+" "
-			+((RationCard) m1.getValue()).getName()
-			+" "+((RationCard) m1.getValue()).getAge()
-			+" "+((RationCard) m1.getValue()).getAddress()
-			+" "+(((RationCard) m1.getValue()).getAl())); 
+			System.out.println(m1.getKey()+" "
+					+((RationCard) m1.getValue()).getName()
+					+" "+((RationCard) m1.getValue()).getAge()
+					+" "+((RationCard) m1.getValue()).getAddress()
+					+" "+(((RationCard) m1.getValue()).getAl())); 
+			
+			 System.out.println();	
+			
 		}
+		
+		
+		Set valueSet = new HashSet(rc.values());
+        System.out.println("Set of unique values");
+        System.out.println(valueSet);
+        System.out.println();		
+		
 			
 		List<Map.Entry<Integer, Object>> list = 
 	               new LinkedList<Map.Entry<Integer, Object>>(); 
 		
 		list.addAll(s1);
 		
-		Collections.sort(list,new Comparator<Map.Entry<Integer, Object>>()
+		System.out.println("Printing the list");
+		for(int i=0;i<list.size();i++)
+		{
+			
+			System.out.println(list.get(i).getValue().toString());
+			System.out.println(" ");
+		}
+		
+		
+				//sorting based on Name
+				Collections.sort(list,new Comparator<Map.Entry<Integer, Object>>()
 				{
 					public int compare(Map.Entry<Integer, Object>IntegerEntry1,
 							Map.Entry<Integer, Object>IntegerEntry2)
@@ -47,12 +91,14 @@ public class MapStore
 	                            .compareTo(((RationCard)IntegerEntry2.getValue()).getName());
 					}
 				});
-			
-			
-				
+				System.out.println("Sorting Based on Name");
 				for(int i=0;i<list.size();i++)
+				{
 					System.out.println(list.get(i).getValue().toString());
-			
+				    System.out.println(" ");
+				}
+				
+				//sorting based on Age
 				Collections.sort(list,new Comparator<Map.Entry<Integer, Object>>()
 				{
 					public int compare(Map.Entry<Integer, Object>IntegerEntry1,
@@ -63,8 +109,12 @@ public class MapStore
 					}
 				});	
 				
+				System.out.println("Sorting Based on Age");
 				for(int i=0;i<list.size();i++)
+				{
 					System.out.println(list.get(i).getValue().toString());
+				    System.out.println(" ");
+				}
 
 	}
 
--- RationCard.java
+++ RationCard.java
@@ -1,63 +1,87 @@
 import java.util.*;
-public class RationCard 
-{
+
+public class RationCard {
+	
+
 	private String name;
 	private Integer age;
 	private String address;
 	
-	List al=new ArrayList();
-	
-	public RationCard(String name, int age, String address,List al)
-	{
+
+	List<RationCard> al = new ArrayList<RationCard>();
+
+	public RationCard(String name, int age, String address, List<RationCard> al) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.address = address;
-		this.al=al;
-		
-			
+		this.al = al;
 	}
-	
-	public List getAl() {
-		System.out.print(al);
+
+	public RationCard(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+		
+	}
+
+	public List<RationCard> getAl() {
 		return al;
 	}
 
-	
-	  public void setAl(List al) 
-	  { 
-		  this.al = al; 
-	  }
-	 
-
-	public void printList()
-	{
-		System.out.print(al);
+	public void setAl(List<RationCard> al) {
+		this.al = al;
 	}
 	
+	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
 	
-	@Override
-	public String toString()
+	public int hashCode()
 	{
-		return name +"  "+ age +"  " +address;
-				
+		int hashcode = 0;
+		hashcode = age*17;
+		hashcode += name.hashCode();
+		return hashcode;
 	}
+	
+	public boolean equals(Object obj)
+	{
+        if (obj instanceof RationCard) {
+            RationCard rc = (RationCard) obj;
+            return (rc.name.equals(this.name) && rc.age == this.age && rc.address==this.address);
+        } else {
+            return false;
+        }
+    }
+
+	@Override
+	public String toString() {
+		return "RationCard [name=" + name + ", age=" + age + ", address=" + address + ", al=" + al + "]";
+	}
+	
+	
+	
 }
