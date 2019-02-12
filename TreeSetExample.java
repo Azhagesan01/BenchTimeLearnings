52a444722fc1124b23e1a881b94a5fa70cc9093a
blackevil2703 <45650551+blackevil2703@users.noreply.github.com>
2019-02-12 18:23:43 +0530

    12/2/2019

--- TreeSetExample.java
+++ TreeSetExample.java
@@ -1,50 +1,36 @@
-package methodExamples;
+package collection;
 import java.util.*;
+public class TreeSetExample {
 
-public class TreeSetExample 
-{
-
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		  TreeSet<String> ts=new TreeSet<String>();  
-		  ts.add("A");  
-		  ts.add("B");  
-		  ts.add("C");  
-		  ts.add("D");  
-		  ts.add("E");
-		  ts.add("F");
-		  
-		  
-		  System.out.println("Traversing TreeSet");
-		  Iterator<String> itr=ts.iterator();  
-		  while(itr.hasNext())
-		  {  
+		TreeSet<String> ts=new TreeSet<String>();
+		ts.add("A");
+		ts.add("F");
+		ts.add("a");
+		ts.add("G");
+		ts.add("c");
+		
+		Iterator<String> itr=ts.iterator();  
+		  while(itr.hasNext()){  
 		   System.out.println(itr.next());  
-		  }  
+		  } 
 		  
-		  System.out.println("Traversing TreeSet in reverse order");
-		  Iterator i=ts.descendingIterator();  
-	         while(i.hasNext())  
-	         {  
-	             System.out.println(i.next());  
-	         }  
+		  ts.add("b");
+		  ts.add("Q");
+		  ts.add("U");
 		  
-		  System.out.println("TreeSet contains : "+ts);
 		  System.out.println("Highest Value: "+ts.pollFirst());  
-	      System.out.println("Lowest Value: "+ts.pollLast()); 
+	      System.out.println("Lowest Value: "+ts.pollLast());
 	      
+	      System.out.println(ts);
 	      
-	      	 System.out.println("Initial Set: "+ts);  
-          
-	         System.out.println("Reverse Set: "+ts.descendingSet());  
-	           
-	         System.out.println("Head Set: "+ts.headSet("C", true));  
-	          
-	         System.out.println("SubSet: "+ts.subSet("A", false, "E", true));  
-	           
-	         System.out.println("TailSet: "+ts.tailSet("C", false));  
-	        
-		  
+	      System.out.println("Reverse Set: "+ts.descendingSet());  
+	      
+	      System.out.println("SubSet: "+ts.subSet("G", "a"));
+	      
+	      System.out.println(ts.ceiling("A"));
+	      System.out.println(ts.floor("G"));
 
 	}
 
